2a7a7f8a9f1a4f724f8a12ee97e8db18c3856077
lqp336699 <lqp336699@gmail.com>
2024-09-12 16:34:51 +0800

    1

--- test.js
+++ test.js
@@ -1,10 +1,33 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const superagent = require('superagent'); //requset请求
+const cheerio = require('cheerio');
+const fs = require('fs')
+let targeturl ='https://www.bilibili.com/tv/?spm_id_from=333.1007.0.0'
 const port = 3000
-app.get('/', (req, res) => {
-    console.log("接受请求")
-    res.send("gg");
+
+app.get('/api/getMovieList', (req, res) => {
+    superagent.get(targeturl).end((err,res2)=>{
+      $ = cheerio.load(res2.text)
+      let fruits = []
+        $('img').each((i,elem)=>{
+          let temp =  {
+            'title':$(elem).find('h3 , a').text
+          }
+          console.log($(elem))
+          fruits.push($(elem).find('span').text()) ;
+        })
+        console.log(fruits)
+
+      // var fruits = [];
+      // $.each((i,elem)=>{
+      //   // fruits[i] = $.img;
+      // })
+      // fruits.join(', ');
+      // console.log(fruits,"nbnbnb")
+      res.send('oo');
+    })
 })
 
 
